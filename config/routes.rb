cb6aafe4ed71558400208b066aff165de06a1aa6
Carlos Monterrosa <cmonterrosa@gmail.com>
2013-03-15 19:28:11 -0600

    + Add a scaffolds for places and distances

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :distances, :collection => {:table => :get}
+  map.resources :places
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
